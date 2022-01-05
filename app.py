ce603c76eed092965e6351aa2d5e9ce27c7b2a77
Thomas Headland <16597457+theadland@users.noreply.github.com>
2022-01-05 09:34:08 -0700

    first update to starter app

--- app.py
+++ app.py
@@ -2,23 +2,50 @@ from flask import Flask, render_template, json
 import os
 import database.db_connector as db
 
+people_from_app_py =[
+{
+    "name": "Thomas",
+    "age": 33,
+    "location": "New Mexico",
+    "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
+}
+]
+
 # Configuration
 
 app = Flask(__name__)
-db_connection = db.connect_to_database()
+# db_connection = db.connect_to_database()
 
 # Routes 
 
 @app.route('/')
 def root():
-    return render_template("main.j2")
+    return render_template("main.j2", people=people_from_app_py)
 
-@app.route('/bsg-people')
-def bsg_people():
-    query = "SELECT * FROM bsg_people;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()
-    return render_template("bsg.j2", bsg_people=results)
+#@app.route('/bsg-people')
+#def bsg_people():
+#    query = "SELECT * FROM bsg_people;"
+#    cursor = db.execute_query(db_connection=db_connection, query=query)
+#    results = cursor.fetchall()
+#    return render_template("bsg.j2", bsg_people=results)
 
 # Listener
 
